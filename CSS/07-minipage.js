7baf4ee8d442210de920821be87cdbd9987c97bf
Muhammad Ahmad <drevex88@gmail.com>
2025-09-25 00:16:05 +0500

    minipage

---
+++ CSS/07-minipage.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+      document.getElementById("mySidebar").classList.add("active");
+      document.getElementById("overlay").classList.add("active");
+    }
+
+    function closeSidebar() {
+      document.getElementById("mySidebar").classList.remove("active");
+      document.getElementById("overlay").classList.remove("active");
+    }
