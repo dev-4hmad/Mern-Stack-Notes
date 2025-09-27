60509e4098cdef6951139ab33aef0caba6cdd3a0
Muhammad Ahmad <drevex88@gmail.com>
2025-09-27 10:02:09 +0500

    methods.js

---
+++ JAVASCRIPT/01-methods.js
@@ -0,0 +1,16 @@
+// Trim method
+let msg = "    Hello    "
+msg.trim()
+console.log(msg)
+
+// prompt
+let password = prompt("Enter password");
+console.log(password.trim())
+
+// repeat
+let abc = "I love coding ";
+console.log(abc.repeat(5));
+
+// splice
+let arr = ['a', 'b'];
+console.log(arr.splice(0, 0, 'c', 'd'));
