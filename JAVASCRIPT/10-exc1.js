d7b1329a47d836321ad650b19e70742a1c71793a
Muhammad Ahmad <drevex88@gmail.com>
2025-09-30 21:09:16 +0500

    number guessing game

---
+++ JAVASCRIPT/10-exc1.js
@@ -0,0 +1,24 @@
+let para = document.querySelector("p");
+let btn = document.querySelector("button");
+let inp = document.querySelector("input")
+
+let num = Math.floor((Math.random()*10)+1)
+console.log(num)
+
+btn.addEventListener('click', ()=>{
+    let guess = Number(inp.value)
+    if (!guess){
+        para.innerText = "enter text"
+    }
+    else if (guess > num) {
+        para.innerText = "too big"
+    } else if (guess < num){
+        para.innerText = "too small"
+    }
+    
+    else {
+        para.innerText = `yeah it was: ${guess}`
+    }
+})
+
+
