ff201eb8243857fba7e29143251b7200ed327d3a
Muhammad Ahmad <drevex88@gmail.com>
2025-10-03 22:04:45 +0500

    update

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,9 +1,11 @@
+// installed things: express, ejs, mongoose, method-override
+
 // express app
 const express = require("express");
 const app = express();
 const path = require("path");
 const Chat = require("./models/chat.js");
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
 // path
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -30,7 +32,7 @@ async function main() {
 
 app.get("/chats", async (req, res) => {
   let chats = await Chat.find();
-//   console.log(chats);
+  //   console.log(chats);
   res.render("index.ejs", { chats });
 });
 
@@ -55,49 +57,34 @@ app.post("/chats", (req, res) => {
     .catch((err) => {
       console.log("err");
     });
-  res.redirect("/chats")
+  res.redirect("/chats");
 });
 
-app.get("/chats/:id/edit", async (req, res)=>{
-    let {id} = req.params;
-    let chat = await Chat.findById(id);
-    res.render("edit.ejs", {chat})
-})
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
 
+app.put("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let { msg: newMsg } = req.body;
 
-app.put("/chats/:id", async (req, res)=>{
-    let {id} = req.params;
-    let {msg: newMsg} = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    { msg: newMsg },
+    { runValidators: true, new: true }
+  );
+  console.log(updatedChat);
+  res.redirect("/chats");
+});
 
-    let updatedChat = await Chat.findByIdAndUpdate(id, {msg: newMsg}, {runValidators: true, new: true})
-    console.log(updatedChat)
-    res.redirect("/chats")
-})
-
-
-app.delete("/chats/:id", async (req, res)=>{
-    let {id} = req.params;
-    let deletedChat = await Chat.findByIdAndDelete(id);
-    console.log(deletedChat)
-    res.redirect("/chats")
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.delete("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedChat = await Chat.findByIdAndDelete(id);
+  console.log(deletedChat);
+  res.redirect("/chats");
+});
 
 app.get("/", (req, res) => {
   res.send("root is working");
--- MongoDB/init.js
+++ MongoDB/init.js
@@ -13,31 +13,6 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
-let allChats = [
-  {
-    from: "Ahmad",
-    to: "Ali",
-    msg: "Hello ali i'm in the uni",
-    created_at: new Date(),
-  },
-  {
-    from: "Javed",
-    to: "Karim",
-    msg: "tommorow is match",
-    created_at: new Date(),
-  },
-  {
-    from: "Fatima",
-    to: "Zainab",
-    msg: "The dish was amazing",
-    created_at: new Date(),
-  },
-  {
-    from: "Tayyab",
-    to: "Sageer",
-    msg: "hahahahah",
-    created_at: new Date(),
-  },
-];
+let allChats = [];
 
 Chat.insertMany(allChats);
