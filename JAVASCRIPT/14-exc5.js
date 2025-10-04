aba4ce12b76c25ba2957ff0e241b13c01f7cedbd
Muhammad Ahmad <drevex88@gmail.com>
2025-10-04 22:02:02 +0500

    modified

--- JAVASCRIPT/14-exc5.js
+++ JAVASCRIPT/14-exc5.js
@@ -1,2 +1,10 @@
-let body = document.body
-body.append("hello")
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+console.log(twoSum([1, 2, 3], 4))
