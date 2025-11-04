7d07988f8b60c27f85ae89921830bfef140a0a2e
dev-4hmad <drevex88@gmail.com>
2025-11-04 11:00:37 +0500

    Add listing and review routes with validation and CRUD functionality

---
+++ 01-PROJECT/routes/listing.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const ExpressError = require("../utils/ExpressError");
+const { listingSchema } = require("../schema.js");
+const Listing = require("../models/listing");
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(404, errMsg);
+  } else {
+    next();
+  }
+};
+
+router.get(
+  "/",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
+
+router.get("/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+router.get(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+// create route
+router.post(
+  "/",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    const listingData = req.body.listing;
+    if (listingData.image && typeof listingData.image === "string") {
+      listingData.image = { filename: "external", url: listingData.image };
+    }
+    const newListing = new Listing(listingData);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
+
+// edit route
+
+router.get(
+  "/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+// update route
+router.put(
+  "/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listingData = req.body.listing;
+    if (listingData.image && typeof listingData.image === "string") {
+      listingData.image = { filename: "external", url: listingData.image };
+    }
+    await Listing.findByIdAndUpdate(id, { ...listingData });
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+router.delete(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
+
+module.exports = router;
